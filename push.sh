27b6bad1a1b7f622c90f44699870a265a4675cee
jdh4 <jdh4@princeton.edu>
2020-09-17 21:59:57 -0400

    Added Adroit

---
+++ push.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp name2id ../../python-utilities
+scp name2id jdh4@adroit.princeton.edu:/home/jdh4/bin
+scp /tigress/jdh4/python-devel/lft/combined_getent.csv jdh4@adroit.princeton.edu:/home/jdh4/bin
