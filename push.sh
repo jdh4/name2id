8a1cc81d1b52e0f739cc9bf5e15632084f7c23f1
jdh4 <jdh4@princeton.edu>
2024-11-14 13:52:21 -0500

    Updates for tiger3

--- push.sh
+++ push.sh
@@ -4,7 +4,7 @@
 cp name2id /home/jdh4/bin
 
 # other clusters
-for cluster in adroit della tigressdata traverse stellar-intel
+for cluster in adroit della tiger3 stellar-intel
 do
   echo ${cluster}
   scp name2id jdh4@${cluster}.princeton.edu:/home/jdh4/bin
